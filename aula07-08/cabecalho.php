62ed103cf8ef271ae009e0e534a410fdd2f46dfa
nosequeponeraquiXD <140538914+nosequeponeraquiXD@users.noreply.github.com>
2023-08-08 15:52:07 -0300

    Delete aula07-08 directory

--- aula07-08/cabecalho.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Aula 07-08</title>
-	<link rel="stylesheet" type="text/css" href="css/estilo.css">
-</head>
-<body>
